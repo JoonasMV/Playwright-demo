0fe767fa10cae118bda9e2e2c42f070e717498a2
Joonas V <JoonasMRViljanen@gmail.com>
2023-04-24 17:56:12 +0300

    browserstack started

--- tests/accessibility.test.js
+++ tests/accessibility.test.js
@@ -4,9 +4,24 @@ const { injectAxe, checkA11y } = require("axe-playwright")
 let browser;
 let page;
 
+const cp = require('child_process');
+const clientPlaywrightVersion = cp.execSync('npx playwright --version').toString().trim().split(' ')[1];  // This is used to get the installed Playwright version on you machine. The same needs to be passed on to BrowserStack so that proper request-response mapping can be done for mismatched client and server Playwright versions in the same test
+
+const caps = {
+  'browser': 'playwright-chromium',
+  'os': 'os x',
+  'os_version': 'mojave',
+  'browserstack.username': process.env.BROWSERSTACK_USERNAME || 'joonasviljanen_W8FAdB',
+  'browserstack.accessKey': process.env.BROWSERSTACK_ACCESS_KEY || 'sANxLpqxoikRsNaubgxU',
+  'client.playwrightVersion': clientPlaywrightVersion
+};
+
 test.describe('Main page validation', () => {
   test.beforeAll(async () => {
-    browser = await chromium.launch()
+    // browser = await chromium.launch()
+    const browser = await chromium.connect({
+      wsEndpoint: `wss://cdp.browserstack.com/playwright?caps=${encodeURIComponent(JSON.stringify(caps))}`
+    });
     page = await browser.newPage()
     await page.goto("https://areena.yle.fi/tv")
 
